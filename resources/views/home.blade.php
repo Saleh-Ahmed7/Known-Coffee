37075fb5a71869a2ab78bb5a0584409d5ae9721f
Saleh-Ahmed7 <113445812+Saleh-Ahmed7@users.noreply.github.com>
2022-11-04 18:48:55 +0300

    frist commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>الصفحة الرئيسية</title>
+    <style>
+        body {
+            background-image: url("backgruond.png");
+        }
+
+        img {
+            width: 1000px;
+            margin: 0px;
+            padding: 0px;
+            margin-left: -1rem;
+            margin-top: -9rem;
+        }
+    </style>
+</head>
+
+<body>
+    <img src="coffee.png" alt="">
+    <img src="knownn.png" alt="" style="
+    margin-top: inherit;
+    margin-left: 15rem;
+    width: 45%">
+    
+</body>
+</html>
+
