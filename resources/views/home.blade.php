2414133f36f0aed856415696696b5e07f133324c
Saleh-Ahmed7 <113445812+Saleh-Ahmed7@users.noreply.github.com>
2022-11-05 06:14:41 +0300

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,11 +22,68 @@
 </head>
 
 <body>
-    <img src="coffee.png" alt="">
+    <img src="coffee.png" alt="" style="
+    width: 102%;
+    margin-top: -45rem">
+
     <img src="knownn.png" alt="" style="
+    
+    margin-top: 3rem;
+    margin-left: 40rem;
+    width: 45%
+    ">
+
+    <h1 style="
+    margin-left: 89rem;
+    font-size: 57px;
+">نقدم لكم القهوة المختصة والمزيد بكل حب</h1>
+    
+    <img src="t1.png" alt="" style="
+   
     margin-top: inherit;
-    margin-left: 15rem;
-    width: 45%">
+    margin-left: 5rem;
+    width: 32%;
+    ">
+    <p style="
+    margin-top: -7rem;
+    margin-left: 13rem;
+    font-size: 58px;
+">
+المشروبات الباردة
+</p>
+
+    <img src="t2.png" alt="" style="
+   
+   margin-top: -50rem;
+   margin-left: 86rem;
+   width: 40%;
+   margin-bottom: 8rem;
+   
+   ">
+    
+    <img src="t3.png" alt="" style="
+    margin-top: -13rem;
+    width: 100%;
+    margin-left: -1rem;
+    
+    ">
+
+    <img src="t4.png" alt="" style="
+   width: 27%;
+   margin-left: 6rem;
+   margin-top: -16rem;
+   
+    ">
+
+    <img src="t5.png" alt="" style="
+   
+   width: 32%;
+   margin-left: 49rem;
+   margin-bottom: -5rem;
+   margin-top: -17rem;
+   
+    ">
+  
     
 </body>
 </html>
