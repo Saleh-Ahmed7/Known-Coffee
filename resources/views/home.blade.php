4e6c30e82f66cd1f47f7c53b57928f9f6fcb0b58
Saleh-Ahmed7 <113445812+Saleh-Ahmed7@users.noreply.github.com>
2022-11-07 21:31:35 +0300

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,22 +30,22 @@
     margin-top: 2rem;
     margin-left: 33rem;
     width: 45%
-    
+
     ">
 
     <h1 style="
     margin-left: 63rem;
     font-size: 33px;
 ">نقدم لكم القهوة المختصة والمزيد بكل حب</h1>
-    
+
     <img src="t1.png" alt="" style="
-   
+
     margin-top: inherit;
     margin-left: 5rem;
     width: 32%;
     ">
     <p style="
-    
+
     margin-top: -5rem;
     margin-left: 8rem;
     font-size: 58px;
@@ -55,42 +55,58 @@
 </p>
 
     <img src="t2.png" alt="" style="
-   
+
    margin-top: -36rem;
    margin-left: 57rem;
    width: 40%;
    margin-bottom: 8rem;
-   
-   
+
+
    ">
-    
+   <p style="
+    margin-left: 67rem;
+    font-size: 58px;
+    margin-top: -11rem;">
+     المشروبات الساخنة
+   </p>
+
     <img src="t3.png" alt="" style="
     margin-top: -13rem;
     width: 100%;
     margin-left: -1rem;
-    
+
     ">
 
     <img src="t4.png" alt="" style="
-   
+
    width: 27%;
    margin-left: 6rem;
    margin-top: -14rem;
-   
-   
+
+
     ">
+<p style="
+    margin-left: 12rem;
+    font-size: 45px;
+    margin-top: 0rem;
+">
+    وجبات الافطار
+    </p>
 
     <img src="t5.png" alt="" style="
-   
-   
+
+
    width: 32%;
    margin-left: 62rem;
    margin-top: -30rem;
-   
-   
     ">
-  
-    
+    <p style="
+    margin-left: 75rem;
+    margin-top: -2rem;
+    font-size: 46px;
+">الحلى </p>
+
+
 </body>
 </html>
 
