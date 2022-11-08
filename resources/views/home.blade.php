d4b19419b6486f42ad0d37c5e06e6c18c0c50bc6
Saleh-Ahmed7 <113445812+Saleh-Ahmed7@users.noreply.github.com>
2022-11-08 21:14:58 +0300

    format to code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,118 +1,120 @@
 <!DOCTYPE html>
+
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>الصفحة الرئيسية</title>
-    <style>
-        body {
-            background-image: url("backgruond.png");
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>الصفحة الرئيسية</title>
+        <style>
+            body {
+                background-image: url("backgruond.png");
+            }
 
-        img {
-            width: 100px;
-            margin: 0px;
-            padding: 0px;
-            margin-left: -1rem;
-            margin-top: -9rem;
-        }
-        .img1{
-            width: 102%;
-            margin-top: -45rem;
-        }
-        .img2{
-            margin-top: 2rem;
-            margin-left: 29rem;
-            width: 45%;
-        }
-        .h1{
-            margin-left: 63rem;
-            font-size: 33px;
-        }
-        .img3{
-            margin-top: inherit;
-            margin-left: 5rem;
-            width: 32%;
-        }
-        .p1{
-            margin-top: -5rem;
-            margin-left: 8rem;
-            font-size: 58px;
-        }
-        .img4{
-            margin-top: -36rem;
-            margin-left: 57rem;
-            width: 40%;
-            margin-bottom: 8rem;
-        }
-        .p2{
-            margin-left: 67rem;
-            font-size: 58px;
-            margin-top: -11rem;
-        }
-        .img5{
-            margin-top: -13rem;
-            width: 100%;
-            margin-left: -1rem;
-        }
-        .img6{
-            width: 27%;
-            margin-left: 6rem;
-            margin-top: -14rem;
-        }
-        .p3{
-            margin-left: 12rem;
-            font-size: 45px;
-            margin-top: 0rem;
-        }
-        .img7{
-            width: 32%;
-            margin-left: 62rem;
-            margin-top: -30rem;
-        }
-        .p4{
-            margin-left: 75rem;
-            margin-top: -2rem;
-            font-size: 46px;
-        }
-    </style>
-</head>
+            img {
+                width: 100px;
+                margin: 0px;
+                padding: 0px;
+                margin-left: -1rem;
+                margin-top: -9rem;
+            }
+            .img1{
+                width: 102%;
+                margin-top: -45rem;
+            }
+            .img2{
+                margin-top: 2rem;
+                margin-left: 29rem;
+                width: 45%;
+            }
+            .h1{
+                margin-left: 63rem;
+                font-size: 33px;
+            }
+            .img3{
+                margin-top: inherit;
+                margin-left: 5rem;
+                width: 32%;
+            }
+            .p1{
+                margin-top: -5rem;
+                margin-left: 8rem;
+                font-size: 58px;
+            }
+            .img4{
+                margin-top: -36rem;
+                margin-left: 57rem;
+                width: 40%;
+                margin-bottom: 8rem;
+            }
+            .p2{
+                margin-left: 67rem;
+                font-size: 58px;
+                margin-top: -11rem;
+            }
+            .img5{
+                margin-top: -13rem;
+                width: 100%;
+                margin-left: -1rem;
+            }
+            .img6{
+                width: 27%;
+                margin-left: 6rem;
+                margin-top: -14rem;
+            }
+            .p3{
+                margin-left: 12rem;
+                font-size: 45px;
+                margin-top: 0rem;
+            }
+            .img7{
+                width: 32%;
+                margin-left: 62rem;
+                margin-top: -30rem;
+            }
+            .p4{
+                margin-left: 75rem;
+                margin-top: -2rem;
+                font-size: 46px;
+            }
+        </style>
 
-<body>
-    <img class="img1" src="coffee.png" alt="">
+    </head>
 
-    <img class="img2" src="knownn.png" alt="">
+        <body>
+            <img class="img1" src="coffee.png" alt="">
 
-    <h1 class="h1">
-    نقدم لكم القهوة المختصة والمزيد بكل حب
-    </h1>
+            <img class="img2" src="knownn.png" alt="">
 
-    <img class="img3" src="t1.png" alt="">
+            <h1 class="h1">
+            نقدم لكم القهوة المختصة والمزيد بكل حب
+            </h1>
 
-    <p class="p1">
-    المشروبات الباردة
-    </p>
+            <img class="img3" src="t1.png" alt="">
 
-    <img class="img4" src="t2.png" alt="">
+            <p class="p1">
+            المشروبات الباردة
+            </p>
+
+            <img class="img4" src="t2.png" alt="">
 
 
-   <p class="p2">
-     المشروبات الساخنة
-   </p>
+            <p class="p2">
+            المشروبات الساخنة
+            </p>
 
-    <img class="img5" src="t3.png" alt="">
+            <img class="img5" src="t3.png" alt="">
 
-    <img class="img6" src="t4.png" alt="">
+            <img class="img6" src="t4.png" alt="">
 
-    <p class="p3">
-    وجبات الافطار
-    </p>
+            <p class="p3">
+            وجبات الافطار
+            </p>
 
-    <img class="img7" src="t5.png" alt="">
+            <img class="img7" src="t5.png" alt="">
 
-    <p class="p4">الحلى </p>
-</body>
+             <p class="p4">الحلى </p>
+        </body>
 </html>
 
