2dad51ce9c34d622a753292e3983d4f87c9dd6de
Saleh-Ahmed7 <113445812+Saleh-Ahmed7@users.noreply.github.com>
2022-11-11 13:05:16 +0300

    add prodect file

---
+++ resources/views/prodect.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>المشروبات الباردة</title>
+        <style>
+         body {
+                background-image: url("backgruond.png");
+            }
+
+        .h1{
+            font-size: 50px;
+            font-size: 50px;
+            margin-left: 73rem;
+
+        }
+
+        </style>
+
+    </head>
+
+        <body>
+
+            <h1 class="h1">المشروبات الباردة</h1>
+        </body>
+</html>
+
